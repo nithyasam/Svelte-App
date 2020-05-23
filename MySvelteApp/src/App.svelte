064d97150b6b18d665933057d062cc0408bf7fed
nsam <nithya.sam@mrcooper.com>
2020-05-23 01:08:25 -0500

    Seperating into different components

--- MySvelteApp/src/App.svelte
+++ MySvelteApp/src/App.svelte
@@ -1,10 +1,12 @@
 <script>
+	import MainPage from '../src/MainPage.svelte';
 	export let name;
+	export let src;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Hello Svelte {name}!</h1>
+	<MainPage src={src}/>
 </main>
 
 <style>
---
+++ MySvelteApp/src/MainPage.svelte
@@ -0,0 +1,7 @@
+<script>
+    export let src;
+</script>
+
+<main>
+	<p><a href={src}>My Portfolio</a></p>
+</main>
